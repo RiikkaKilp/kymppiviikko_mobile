1471c8b77afec5a2e69413693ca9c5dca588060a
RiikkaKilp <RiikkaKilp@users.noreply.github.com>
2018-03-02 22:38:47 +0200

    Placeholder prefabs and scripts
    Placeholder tile prefabs, start of some scripts(WIP).

---
+++ Kymppiviikko2018/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    ObjectPooler        objectPooler;
+        
+    GameObject[]        allTiles;
+
+    public GameObject   leftTile, 
+                        rightTile, 
+                        straightTile;
+    GameObject          currentTile,
+                        newTile;
+
+    Vector3             currentPosition,
+                        destroyBox;
+
+    void Start ()
+    {
+        allTiles = new GameObject[] { leftTile, rightTile, straightTile };
+
+        objectPooler = ObjectPooler.Instance;
+
+        currentTile = objectPooler.SpawnFromPool("StraightPiece", transform.position, Quaternion.identity);
+        newTile = objectPooler.SpawnFromPool("StraightPiece", newTile.transform.GetChild(0).position, Quaternion.identity);
+       
+	}
+}
